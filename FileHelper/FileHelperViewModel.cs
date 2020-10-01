2052b178971f6640d8b1e021d7eea4a44b391ffe
Reuben Wilson <reuben.h.wilson@gmail.com>
2020-10-01 08:10:32 -0700

    Initial src

---
+++ FileHelper/FileHelperViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Input;
+
+namespace FileHelper
+{
+
+
+    public class FileHelperViewModel
+    {
+
+        private readonly Dictionary<string, string> _fileData;
+
+      
+        public ICommand GetFilesCommand { get; set;  }
+        public ICommand GetFolderCommand { get; set; }
+
+        public string FilePath { get; set; }
+
+        public int FileCount { get; set; }
+
+        public Dictionary<string, string> FileData
+        {
+            get
+            {
+                return _fileData;
+            }
+        }
+
+        public FileHelperViewModel()
+        {
+            _fileData = new Dictionary<string, string>();
+
+            GetFilesCommand = new RelayCommand(GetFiles, CanGetFiles);
+
+            GetFolderCommand = new RelayCommand(GetFolder, CanGetFolder);
+        }
+
+        private bool CanGetFiles(object parameter) { return true;  }
+
+        public void GetFiles(object parameter)
+        {
+            if (FilePath.Length == 0)
+                return;
+
+            try
+            {
+
+                if (Directory.Exists(FilePath))
+                {
+                    foreach (var file in Directory.EnumerateFiles(FilePath))
+                    {
+                        _fileData.Add(file, file);
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+
+            }
+        }
+
+        private bool CanGetFolder(object parameter)
+        {
+            return true;
+        }
+        private void GetFolder(object parameter)
+        {
+            var dialog = new CommonOpenFileDialog();
+
+
+            dialog.InitialDirectory = Directory.GetCurrentDirectory();
+            dialog.IsFolderPicker = true;
+
+            var result =
+                dialog.ShowDialog();
+
+            if (result == CommonFileDialogResult.Ok)
+            {
+                FilePath = dialog.FileName;
+            }
+        }
+    }
+}
---
+++ FileHelper/RelayCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FileHelper
+{
+    public class RelayCommand : ICommand
+    {
+        Action<object> _execteMethod;
+        Func<object, bool> _canexecuteMethod;
+
+        public RelayCommand(Action<object> execteMethod, Func<object, bool> canexecuteMethod)
+        {
+            _execteMethod = execteMethod;
+            _canexecuteMethod = canexecuteMethod;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canexecuteMethod != null)
+            {
+                return _canexecuteMethod(parameter);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execteMethod(parameter);
+        }
+    }
+}
